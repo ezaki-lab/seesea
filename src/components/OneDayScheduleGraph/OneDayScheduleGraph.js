04ab66c529138f0a5fa440565d9d8d60272b6e4c
Kaito Hattori <kaichan394@gmail.com>
2019-07-04 18:12:32 +0900

    Add heatmap chart; using react-apexcharts

--- src/components/OneDayScheduleGraph/OneDayScheduleGraph.js
+++ src/components/OneDayScheduleGraph/OneDayScheduleGraph.js
@@ -1,38 +1,11 @@
 import React, { Component } from 'react'
-import { ComposedChart, Legend, Bar, LabelList, AreaChart, Area, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import ReactApexChart from 'react-apexcharts'
 
 import { LoadingOverlay, Loader } from 'react-overlay-loader';
 
-import 'react-overlay-loader/styles.css';
+import 'react-calendar-heatmap/dist/styles.css';
 import '../Base/Base.css'
 import './OneDayScheduleGraph.css'
-
-// const data = [
-//     { month: "0", tide: 140, feed: 0},
-//     { month: "1", tide: 90, feed: 0},
-//     { month: "2", tide: 60, feed: 0},
-//     { month: "3", tide: 30, feed: 0},
-//     { month: "4", tide: 50, feed: 0},
-//     { month: "5", tide: 100, feed: 0},
-//     { month: "6", tide: 160, feed: 300},
-//     { month: "7", tide: 210, feed: 500},
-//     { month: "8", tide: 260, feed: 400},
-//     { month: "9", tide: 300, feed: 0},
-//     { month: "10", tide: 280, feed: 0},
-//     { month: "11", tide: 230, feed: 0},
-//     { month: "12", tide: 180, feed: 0},
-//     { month: "13", tide: 130, feed: 0},
-//     { month: "14", tide: 80, feed: 0},
-//     { month: "15", tide: 30, feed: 0},
-//     { month: "16", tide: 60, feed: 0},
-//     { month: "17", tide: 110, feed: 0},
-//     { month: "18", tide: 160, feed: 300},
-//     { month: "19", tide: 210, feed: 200},
-//     { month: "20", tide: 260, feed: 0},
-//     { month: "21", tide: 290, feed: 0},
-//     { month: "22", tide: 250, feed: 0},
-//     { month: "23", tide: 200, feed: 0},
-// ]
 
 const RequestStateType = {
     success: 'success',
@@ -52,15 +25,112 @@ class OneDayScheduleGraph extends Component {
             },
             requestStatus: RequestStateType.none,
             schedule: {
-                tides: [],
                 hourFeeds: [0, 0, 0, 0, 0, 0, 300, 500, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 300, 200, 0, 0, 0, 0]
-            }
+            },
+            series: [
+                {
+                    name: '0',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '1',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '2',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '3',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '4',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '5',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '6',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '7',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '8',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '9',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '10',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '11',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '12',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '13',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '14',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '15',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '16',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '17',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '18',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '19',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '20',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '21',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '22',
+                    data: [...Array(60).keys()]
+                },
+                {
+                    name: '23',
+                    data: [...Array(60).keys()]
+                }
+            ],
         };
     }
 
     componentDidMount() {
         this.getComponentSize();
-        this.getTides(this.props.date);
+        // this.getTides(this.props.date);
     }
 
     componentWillMount() {
@@ -82,12 +152,13 @@ class OneDayScheduleGraph extends Component {
             return true;
         }
         return false;
+        // return true;
     }
 
     componentDidUpdate() {
         // For get tides of selected date
         if (this.state.requestStatus === RequestStateType.none) {
-            this.getTides(this.props.date);
+            // this.getTides(this.props.date);
         }
         // For re-render component
         else if (this.state.requestStatus !== RequestStateType.loading) {
@@ -97,28 +168,10 @@ class OneDayScheduleGraph extends Component {
         }
     }
 
-    makeDataPointsFromSchedule(schedule) {
-        var dataPoints = []
-        for (var i = 0; i < 24; i++) {
-            var tide = 0;
-            var hourFeed = 0;
-            if (i < schedule.tides.length) {
-                tide = schedule.tides[i];
-            }
-            if (i < schedule.hourFeeds.length) {
-                hourFeed = schedule.hourFeeds[i];
-            }
-            var point = {
-                month: i.toString(), tide: tide, hourFeed: hourFeed
-            };
-            dataPoints.push(point);
-        }
-        return dataPoints;
-    }
-
     getComponentSize() {
         let height = document.getElementById('seesea-onedaygraph').clientHeight;
         let width = document.getElementById('seesea-onedaygraph').clientWidth;
+        console.log("kita", width, height);
         this.setState({
             size: {
                 height: height,
@@ -127,47 +180,8 @@ class OneDayScheduleGraph extends Component {
         });
     }
 
-    getTides(date) {
-        console.log("request get tides from https://tide-api-ezaki-lab.herokuapp.com/")
-        this.setState({
-            requestStatus: RequestStateType.loading
-        });
-
-        var url_calculate = "https://tide-api-ezaki-lab.herokuapp.com/calculate"
-        var tides_calculate = [];
-        fetch(url_calculate, {
-            method: 'POST',
-            body: JSON.stringify({ 'date': date }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            if (response.status === 201) {
-                return response.json();
-            }
-        }).then(json => {
-            if (json != null) {
-                console.log("success to fetch tides (calculate)");
-                tides_calculate = json['tides'];
-            }
-            else {
-                console.error("failed to fetch tides (calculate)")
-            }
-        }).catch(error => {
-            console.error('Error:', error);
-        }).finally(() => {
-            this.setState({
-                schedule: {
-                    tides: tides_calculate,
-                    hourFeeds: this.state.schedule.hourFeeds
-                },
-                requestStatus: RequestStateType.success
-            });
-        });
-    }
-
     render() {
-        let dataPoints = this.makeDataPointsFromSchedule(this.state.schedule);
+        // let dataPoints = this.makeDataPointsFromSchedule(this.state.schedule);
         let height = this.state.size.height;
         let width = this.state.size.width;
 
@@ -175,29 +189,41 @@ class OneDayScheduleGraph extends Component {
         if (this.state.requestStatus === RequestStateType.loading) {
             isLoadingActive = true;
         }
-        console.log(isLoadingActive);
+
+        var options = {
+            chart: {
+                toolbar: {
+                    show: false
+                },
+                animations: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            colors: ["#008FFB"],
+            xaxis: {
+                type: 'numeric',
+                tickAmount: 6,
+                labels: {
+                    formatter: function (value) {
+                        return Math.round(value - 1);
+                    }
+                }
+            }
+        };
 
         return (
             <div id='seesea-onedaygraph' className='base'>
-                <LoadingOverlay style={{ width: width, height: height }}>
-                    <span className='base-title-large'>給餌スケジュール</span>
-                    <ComposedChart width={width} height={height} data={dataPoints} margin={{ top: 60, right: 30, bottom: 10, left: 0 }}>
-                        <defs>
-                            <linearGradient id="gradient-tide" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#F18DE9" stopOpacity={0.5} />
-                                <stop offset="95%" stopColor="#CE2BC1" stopOpacity={0.0} />
-                            </linearGradient>
-                        </defs>
-                        <CartesianGrid stroke="#8097B1" strokeOpacity={0.4} strokeWidth={0.5} strokeDasharray="3 5" />
-                        <Area dataKey="tide" type="monotone" stroke="#F18DE9" fillOpacity={1} fill="url(#gradient-tide)" />
-                        <Bar dataKey='hourFeed' barSize={10} fill="#3794FC" />
-                        <XAxis dataKey="month" stroke="#E0E7FF" tick={{ fill: "#97A4BA" }} />
-                        <YAxis stroke="#E0E7FF" tick={{ fill: "#97A4BA" }} />
-                        <Tooltip />
-                        <Legend />
-                    </ComposedChart>
+                <span className='base-title-large'>給餌スケジュール</span>
+                <div className='chart'>
+                    <ReactApexChart options={options} series={this.state.series} width="100%" height="100%" type={'heatmap'} />
+                </div>
+                {/* <LoadingOverlay style={{ width: width, height: height }}>
+                    
                     <Loader loading={isLoadingActive} />
-                </LoadingOverlay>
+                </LoadingOverlay> */}
             </div>
         )
     }
